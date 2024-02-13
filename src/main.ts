84acedf694fbdf4bfdbe9cf412f6adc4888651de
Vlad Ovčaruk <vladovcharyk@gmail.com>
2024-02-13 21:21:30 +0300

    add https

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import * as fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync('./cert/key.pem'),
+    cert: fs.readFileSync('./cert/cert.pem'),
+  };
+  const app = await NestFactory.create(AppModule,{
+    httpsOptions
+  });
 
   app.setGlobalPrefix('api');
 
